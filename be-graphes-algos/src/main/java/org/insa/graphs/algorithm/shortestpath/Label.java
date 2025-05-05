1f0b4a1d8612f497cb144c77544697ca4ad58abe
Crouzet Nubia <crouze@insa-20657>
2025-05-05 10:51:49 +0200

    modif type sommetCourant

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -1,22 +1,23 @@
 package org.insa.graphs.algorithm.shortestpath;
 
 import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Node;
 
 public class Label implements Comparable<Label> {
 
-    private int sommetCourant ; //sommet associé au label
+    private Node sommetCourant ; //sommet associé au label
     private boolean marque ; //vrai lorsque coût min de ce sommet est définitivement connu par l'algorithme
     private double coutRealise ; //valeur courante du plus court chemin depuis l'origine vers le sommet
     private Arc pere ; //sommet précédent sur le chemin correspondant au plus court chemin
 
-    public Label(int sommetCourant, boolean marque, double coutRealise, Arc pere) {
+    public Label(Node sommetCourant, boolean marque, double coutRealise, Arc pere) {
         this.sommetCourant = sommetCourant ;
         this.marque = marque ;
         this.coutRealise = coutRealise ;
         this.pere = pere ;
     }
 
-    public int getSommetCourant(){ return this.sommetCourant; } 
+    public Node getSommetCourant(){ return this.sommetCourant; } 
     public boolean getMarque(){ return this.marque; } 
     public double getCoutRealise(){ return this.coutRealise; } 
     public Arc getPere(){ return this.pere; } 
